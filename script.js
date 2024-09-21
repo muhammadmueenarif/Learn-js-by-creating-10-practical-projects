bee895a9bd954dc498a0d6c1e36f41fe5e886041
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-09-21 10:06:30 +0500

    project 4. HEX Color Generator. complete

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateColor() {
+    
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        document.getElementsByClassName('color-box')[0].style.backgroundColor = color;
+        document.getElementById('hex-color-code').innerHTML = color;
+    }
+    return color;
+}
